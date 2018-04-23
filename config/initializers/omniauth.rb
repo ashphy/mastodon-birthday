111e3753e567fa6cd0c9cb6d4096cd05c7c7a1ef
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2018-04-24 00:06:32 +0900

    add osushi

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :mastodon, scope: 'write', credentials: lambda { |domain, callback_url|
+  provider :mastodon, scope: 'read write', credentials: lambda { |domain, callback_url|
     Rails.logger.info "Requested credentials for #{domain} with callback URL #{callback_url}"
 
     instance = Instance.first_or_create(host: domain) do |ins|
       Rails.logger.info 'Instance not found. Creating app for mastodon instance.'
       client = Mastodon::REST::Client.new(base_url: "https://#{domain}")
-      app = client.create_app('MastodonBirthday', callback_url, 'read')
+      app = client.create_app('MastodonBirthday', callback_url, 'read write')
       ins.client_id = app.client_id
       ins.client_secret = app.client_secret
     end
