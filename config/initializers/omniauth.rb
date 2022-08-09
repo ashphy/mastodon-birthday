54797f6e5c9682bcf5864a25dd7596f7b5229ce4
Kazuki Hamasaki <kazuki-h@iij.ad.jp>
2022-08-09 18:08:55 +0900

    fix oauth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :mastodon, scope: 'read write', credentials: lambda { |domain, callback_url|
+  provider :mastodon, scope: 'read', credentials: lambda { |domain, callback_url|
     Rails.logger.info "Requested credentials for #{domain} with callback URL #{callback_url}"
 
     instance = Instance.find_or_create_by(host: domain) do |ins|
